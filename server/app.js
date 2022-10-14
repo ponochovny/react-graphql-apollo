e94c60d2d4832d6087427277fcdd3fa7a4c1fd19
ponochovny <ponochovny@gmail.com>
2022-10-15 02:59:36 +0300

    Mongoose requests

--- server/app.js
+++ server/app.js
@@ -17,6 +17,10 @@ app.use(
 	})
 )
 
+const dbConnection = mongoose.connection
+dbConnection.on('error', (err) => console.log(`Connection error: ${err}`))
+dbConnection.once('open', () => console.log(`Connected to DB!`))
+
 app.listen(PORT, (err) => {
 	err ? console.log(err) : console.log('Server started!')
 })
