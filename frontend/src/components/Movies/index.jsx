7c40e9eabe11229fcb3b27b4ec7710df60857069
ponochovny <ponochovny@gmail.com>
2022-10-15 09:08:39 +0300

    mutation query

--- frontend/src/components/Movies/index.jsx
+++ frontend/src/components/Movies/index.jsx
@@ -1,11 +1,43 @@
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql, useMutation } from '@apollo/client'
 
-const GET_LOCATIONS = gql`
+const GET_MOVIES = gql`
 	query moviesQuery {
 		movies {
 			id
 			name
 			genre
+			director {
+				name
+			}
+			watched
+			rate
+		}
+	}
+`
+const SET_WATCHED = gql`
+	mutation (
+		$id: ID
+		$name: String!
+		$genre: String!
+		$directorId: ID
+		$watched: Boolean!
+		$rate: Int
+	) {
+		updateMovie(
+			id: $id
+			name: $name
+			genre: $genre
+			directorId: $directorId
+			watched: $watched
+			rate: $rate
+		) {
+			name
+			genre
+			director {
+				name
+			}
+			watched
+			rate
 		}
 	}
 `
@@ -15,9 +47,27 @@ const Movies = () => {
 		loading,
 		error,
 		data: { movies = undefined } = {},
-	} = useQuery(GET_LOCATIONS)
+	} = useQuery(GET_MOVIES)
+	const [
+		updateMovie,
+		{ data, loading: loadingUPDMovie, error: errorUPDMovie },
+	] = useMutation(SET_WATCHED)
 
 	console.log(movies)
+
+	const updateMovieFunc = (el) => {
+		console.log(el.watched)
+		updateMovie({
+			variables: {
+				id: el.id,
+				name: el.name,
+				genre: el.genre,
+				directorId: el.directorId,
+				watched: !el.watched,
+			},
+			refetchQueries: [{ query: GET_MOVIES }],
+		})
+	}
 
 	return (
 		<div>
@@ -25,7 +75,38 @@ const Movies = () => {
 			{loading ? (
 				<p>Loading...</p>
 			) : (
-				<ul>{movies && movies.map((el) => <li key={el.id}>{el.name}</li>)}</ul>
+				<table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Genre</th>
+							<th>Rate</th>
+							<th>Director</th>
+							<th>Watched</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						{movies &&
+							movies.map((el) => (
+								<tr key={el.id}>
+									<td>{el.name}</td>
+									<td>{el.genre}</td>
+									<td>{el.rate ? el.rate : 'not voted'}</td>
+									<td>{el.director.name}</td>
+									<td
+										className='watching'
+										onClick={() => {
+											updateMovieFunc(el)
+										}}
+									>
+										{el.watched ? 'Yes' : 'No'}
+									</td>
+									<td>Edit btn</td>
+								</tr>
+							))}
+					</tbody>
+				</table>
 			)}
 		</div>
 	)
