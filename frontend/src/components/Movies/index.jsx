caf1ebbb4056ff2ad0c1d616e2f994473c532b9f
ponochovny <ponochovny@gmail.com>
2022-10-17 03:48:38 +0300

    Complete update Movie

--- frontend/src/components/Movies/index.jsx
+++ frontend/src/components/Movies/index.jsx
@@ -1,18 +1,32 @@
 import { useQuery, gql, useMutation } from '@apollo/client'
 import {
 	Box,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	FormControlLabel,
 	IconButton,
+	MenuItem,
 	Paper,
+	Rating,
+	Switch,
 	Table,
 	TableBody,
 	TableCell,
 	TableContainer,
 	TableHead,
 	TableRow,
+	TextField,
 	Typography,
 } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit'
-import { green, grey, red } from '@mui/material/colors'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import { grey } from '@mui/material/colors'
+import { useState } from 'react'
+import { Stack } from '@mui/system'
+import { MyRating } from '../MyRating/MyRating'
 
 const GET_MOVIES = gql`
 	query moviesQuery {
@@ -21,6 +35,7 @@ const GET_MOVIES = gql`
 			name
 			genre
 			director {
+				id
 				name
 			}
 			watched
@@ -55,29 +70,33 @@ const SET_WATCHED = gql`
 		}
 	}
 `
+const GET_DIRECTORS = gql`
+	query directorsQuery {
+		directors {
+			id
+			name
+			age
+		}
+	}
+`
 
 const Movies = () => {
-	const {
-		loading,
-		error,
-		data: { movies = undefined } = {},
-	} = useQuery(GET_MOVIES)
-	const [
-		updateMovie,
-		{ data, loading: loadingUPDMovie, error: errorUPDMovie },
-	] = useMutation(SET_WATCHED)
+	const { data: { directors = undefined } = {} } = useQuery(GET_DIRECTORS)
+	const { loading, data: { movies = undefined } = {} } = useQuery(GET_MOVIES)
+	const [updateMovie] = useMutation(SET_WATCHED)
 
-	console.log(movies)
+	const [open, setOpen] = useState(false)
+	const [editableMovie, setEditableMovie] = useState(null)
 
-	const updateMovieFunc = (el) => {
-		console.log(el.watched)
+	const updateMovieFunc = () => {
 		updateMovie({
 			variables: {
-				id: el.id,
-				name: el.name,
-				genre: el.genre,
-				directorId: el.directorId,
-				watched: !el.watched,
+				id: editableMovie.id,
+				name: editableMovie.name,
+				genre: editableMovie.genre,
+				directorId: editableMovie.directorId,
+				rate: editableMovie.rate,
+				watched: editableMovie.watched,
 			},
 			refetchQueries: [{ query: GET_MOVIES }],
 		})
@@ -119,30 +138,35 @@ const Movies = () => {
 									<TableRow hover key={el.id}>
 										<TableCell>{el.name}</TableCell>
 										<TableCell>{el.genre}</TableCell>
-										<TableCell>{el.rate ? el.rate : 'not voted'}</TableCell>
+										<TableCell>
+											<Rating value={el.rate} readOnly />
+										</TableCell>
 										<TableCell>{el.director.name}</TableCell>
-										<TableCell
-											align='center'
-											sx={[
-												{
-													bgcolor: el.watched ? green[300] : red[300],
-													transition: 'background-color .3s ease',
-												},
-												{
-													'&:hover': {
-														bgcolor: el.watched ? green[200] : red[200],
-														cursor: 'pointer',
-													},
-												},
-											]}
-											onClick={() => {
-												updateMovieFunc(el)
-											}}
-										>
-											<Box>{el.watched ? 'Yes' : 'No'}</Box>
+										<TableCell align='center'>
+											<Box>
+												{el.watched ? (
+													<VisibilityIcon fontSize='small' color='action' />
+												) : (
+													<VisibilityIcon fontSize='small' color='disabled' />
+												)}
+											</Box>
 										</TableCell>
 										<TableCell>
-											<IconButton aria-label='edit' size='small'>
+											<IconButton
+												onClick={() => {
+													setOpen(true)
+													setEditableMovie({
+														id: el.id,
+														name: el.name,
+														genre: el.genre,
+														directorId: el.director.id,
+														rate: el.rate,
+														watched: el.watched,
+													})
+												}}
+												aria-label='edit'
+												size='small'
+											>
 												<EditIcon fontSize='medium' />
 											</IconButton>
 										</TableCell>
@@ -152,6 +176,105 @@ const Movies = () => {
 					</Table>
 				</TableContainer>
 			)}
+
+			<Dialog
+				open={open}
+				onClose={() => setOpen(false)}
+				aria-labelledby='dialog-title'
+				aria-describedby='dialog-description'
+			>
+				<DialogTitle id='dialog-title'>Movie Editor</DialogTitle>
+				<DialogContent sx={{ minWidth: '350px' }}>
+					<Stack marginBottom={2} marginTop={1}>
+						<TextField
+							label='Name'
+							value={editableMovie?.name}
+							size='small'
+							onChange={(e) => {
+								setEditableMovie((prev) => {
+									return { ...prev, name: e.target.value }
+								})
+							}}
+						/>
+					</Stack>
+					<Stack marginBottom={2}>
+						<TextField
+							label='Genre'
+							size='small'
+							value={editableMovie?.genre}
+							onChange={(e) => {
+								setEditableMovie((prev) => {
+									return { ...prev, genre: e.target.value }
+								})
+							}}
+						/>
+					</Stack>
+					<Stack marginBottom={1}>
+						<TextField
+							label='Select director'
+							select
+							value={editableMovie?.directorId}
+							onChange={(e) =>
+								setEditableMovie((prev) => {
+									return {
+										...prev,
+										directorId: e.target.value,
+									}
+								})
+							}
+							fullWidth
+						>
+							{directors &&
+								directors.map((director) => (
+									<MenuItem key={director.id} value={director.id}>
+										{director.name}
+									</MenuItem>
+								))}
+						</TextField>
+					</Stack>
+					<Stack>
+						<FormControlLabel
+							label='Watched?'
+							control={
+								<Switch
+									checked={editableMovie?.watched}
+									onChange={(e) => {
+										setEditableMovie((prev) => {
+											return {
+												...prev,
+												watched: e.target.checked,
+											}
+										})
+									}}
+								/>
+							}
+						/>
+					</Stack>
+					<Stack>
+						<MyRating
+							changed={(newVal) => {
+								setEditableMovie((prev) => {
+									return { ...prev, rate: newVal }
+								})
+							}}
+							initialValue={editableMovie?.rate}
+						/>
+					</Stack>
+				</DialogContent>
+				<DialogActions sx={{ px: 3 }}>
+					<Button onClick={() => setOpen(false)}>Cancel</Button>
+					<Button
+						autoFocus
+						onClick={() => {
+							setOpen(false)
+							updateMovieFunc()
+						}}
+						variant='contained'
+					>
+						Submit
+					</Button>
+				</DialogActions>
+			</Dialog>
 		</Box>
 	)
 }
