49101a81294c1c4a403c24de74dc77cb0dcf6e4e
ponochovny <ponochovny@gmail.com>
2022-10-21 12:42:56 +0300

    small fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 
-const isProduction = true
+const isDev = true
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<>
-		{isProduction ? (
+		{!isDev ? (
 			<App />
 		) : (
 			<React.StrictMode>
