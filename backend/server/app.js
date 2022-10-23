fd35ab4fc050f297a3b38d13f394309793ddf86f
ponochovny <ponochovny@gmail.com>
2022-10-23 06:48:54 +0300

    backend fix

--- backend/server/app.js
+++ backend/server/app.js
@@ -5,12 +5,19 @@ const schema = require('../schema/schema')
 const mongoose = require('mongoose')
 const cors = require('cors')
 
+const isProduction = true
+
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true })
 
 const app = express()
 const PORT = 3005
+const corsOptions = isProduction
+	? {
+			origin: 'https://react-graphql-movies.netlify.app',
+	  }
+	: null
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(
 	'/graphql',
@@ -24,6 +31,6 @@ const dbConnection = mongoose.connection
 dbConnection.on('error', (err) => console.log(`Connection error: ${err}`))
 dbConnection.once('open', () => console.log(`Connected to DB!`))
 
-app.listen(PORT, (err) => {
+app.listen(process.env.PORT || PORT, (err) => {
 	err ? console.log(err) : console.log('Server started!')
 })
